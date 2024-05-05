9a5b99f2aaa1df7c1e435cc0b4cf15c08034bde9
Michal Cieslar <michalovskyyy@gmail.com>
2024-05-05 21:58:55 +0200

    add http server to application

--- src/application.ts
+++ src/application.ts
@@ -1,20 +1,25 @@
 import { type Logger } from './common/logger/logger.js';
 import { LoggerFactory } from './common/logger/loggerFactory.js';
-import { type Config, ConfigFactory } from './config.js';
+import { ConfigFactory } from './config.js';
+import { HttpServer } from './httpServer.js';
 
 export class Application {
-  private config: Config | undefined;
   private logger: Logger | undefined;
+  private httpServer: HttpServer | undefined;
 
   public async start(): Promise<void> {
-    this.config = ConfigFactory.create();
+    const config = ConfigFactory.create();
 
-    this.logger = LoggerFactory.create({ logLevel: this.config.logLevel });
+    this.logger = LoggerFactory.create({ logLevel: config.logLevel });
+
+    this.httpServer = new HttpServer(this.logger, config);
 
     this.logger.info({ message: 'Application started.' });
   }
 
   public async stop(): Promise<void> {
+    await this.httpServer?.stop();
+
     this.logger?.info({ message: 'Application stopped.' });
   }
 }
